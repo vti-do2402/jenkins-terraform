4cf4844213abef1f9e4d7872bf00546550ea01e6
Quentin <s3981278@rmit.edu.vn>
2025-03-19 11:22:35 +0700

    Add backend

--- .infra/environment/dev/data.tf
+++ .infra/environment/dev/data.tf
@@ -1,2 +1,1 @@
 data "aws_availability_zones" "available" {}
-
--- .infra/environment/dev/providers.tf
+++ .infra/environment/dev/providers.tf
@@ -5,8 +5,16 @@ terraform {
       version = "~> 5.70.0"
     }
   }
+
+  backend "s3" {
+    bucket = "jenkins-terraform-state"
+    key = "dev/terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "jenkins-terraform-state-lock"
+  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
+
