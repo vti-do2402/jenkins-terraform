c16735fdd55782b4ee2118ae2c4176e71d3843c4
Quentin <s3981278@rmit.edu.vn>
2025-03-20 16:03:32 +0700

    add backend for test terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.70.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "quentin-jenkins-terraform-state"
+    key            = "test/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "quentin-jenkins-terraform-state-lock"
+  }
 }
 
 provider "aws" {
